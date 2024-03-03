b4ba473759c4c5a6ee15b7d59346dd4c564b8300
Diana-yjh <projin1234@gmail.com>
2024-03-03 23:22:55 +0900

    feat: 숫자 3자리마다 , 추가하도록 extension에 함수 추가

--- Calculator/Models/OperandFormatter.swift
+++ Calculator/Models/OperandFormatter.swift
@@ -5,4 +5,34 @@
 //  Created by Yejin Hong on 3/3/24.
 //
 
-import Foundation
+func operandFormatter(with operand: String) -> String {
+    let integer = operand.split(with: ".").first ?? ""
+    var decimal = ""
+    
+    if operand.contains(".") {
+        decimal = "." + (operand.split(with: ".").last ?? "")
+    }
+    
+    var interval = 1
+    var integerReversed = ""
+    var result = ""
+    
+    for character in integer.reversed() {
+        integerReversed += String(character)
+        
+        if interval == 3 {
+            integerReversed += ","
+            interval = 0
+        }
+
+        interval += 1
+    }
+    
+    result = integerReversed.reversed().map{ String($0) }.joined()
+    
+    if integer.count % 3 == 0 {
+        result.removeFirst()
+    }
+    
+    return result + decimal
+}
