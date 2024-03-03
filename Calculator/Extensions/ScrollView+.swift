372fb86b8cc4f7600ad7b6533cd0e59bfd3bcdbb
Diana-yjh <projin1234@gmail.com>
2024-03-03 23:09:25 +0900

    feat: 3자리 숫자마다 "," 추가, 숫자 20자리까지만 입력 가능, "."이 입력되어 있는 경우 재 입력 불가능, 계산진행 시 로그 scrollView에 추가, scrollView 항상 Bottom 유지하도록

---
+++ Calculator/Extensions/ScrollView+.swift
@@ -0,0 +1,8 @@
+//
+//  ScrollView+.swift
+//  Calculator
+//
+//  Created by Yejin Hong on 2024/03/01.
+//
+
+import Foundation
---
+++ Calculator/Models/OperandFormatter.swift
@@ -0,0 +1,8 @@
+//
+//  OperandFormatter.swift
+//  Calculator
+//
+//  Created by Yejin Hong on 3/3/24.
+//
+
+import Foundation
